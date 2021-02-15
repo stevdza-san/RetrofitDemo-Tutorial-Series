dfd50443923cc3b8ec9b7e8e8d1dc1a51ba0692f
Chris Athanas <chris@crowdskout.com>
2021-02-15 15:23:40 -0600

    Add MyInterceptor.kt and class

---
+++ app/src/main/java/com/example/retrofitdemo/api/MyHeaders.kt
@@ -0,0 +1,16 @@
+package com.example.retrofitdemo.api
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class MyInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+            .newBuilder()
+            .addHeader("Content-Type", "application/json")
+            .addHeader("X-Platform", "Android")
+            .addHeader("X-Auth-Token", "123456789")
+            .build()
+        return chain.proceed(request)
+    }
+}
